09ebf63cb0b3f7ffeab5f95396ee273a1029ca24
Nezaal Ahmad <149879563+Nezaal@users.noreply.github.com>
2026-09-01 00:11:16 +0530

    Time: 22 ms (31.83%), Space: 47.2 MB (16.43%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -1,32 +1,46 @@
 class Solution {
     public int characterReplacement(String s, int k) {
-        
-        HashMap<Character , Integer> map = new HashMap<>();
         int n = s.length();
-        int maxLen = 0;
+        HashMap<Character, Integer> map = new HashMap<>();
         int maxFreq = 0;
+        int maxLen = 0;
         int i = 0;
-
         for(int j = 0; j < n; j++){
-
             char ch = s.charAt(j);
-            int freq = map.getOrDefault(ch, 0)+1;
-            map.put(ch, freq);
+            int freq = map.getOrDefault(ch,0)+1;
+             
             maxFreq = Math.max(maxFreq, freq);
+            map.put(ch, freq);
 
-            while( j-i+1 - maxFreq > k){
+            while(j-i+1 - maxFreq > k){
                 char leftCh = s.charAt(i);
 
-                map.put(leftCh , map.get(leftCh)-1);
+                map.put(leftCh, map.get(leftCh)-1);
                 if(map.get(leftCh) == 0){
                     map.remove(leftCh);
-                }   
+                }
                 i++;
+
             }
-            maxLen = Math.max(maxLen, j-i+1);
+
+
+            maxLen = Math.max(maxLen , j-i+1);
+            
         }
         return maxLen;
-
     }
 }
 
+// // 0 1 2 3 4 5 6
+// // A A B A B B A
+// //   i
+// //         j
+
+// // k = 1
+// freq of A= 2
+// frwq of B= 2
+
+// longest  = 2
+
+
+// windowSize - maxFeq < k
